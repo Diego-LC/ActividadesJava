6a061510a2e7cc4a4d5e35f79658bc265ba6ce8b
DIΣGӨ L <di3golabrin@gmail.com>
2023-08-23 13:58:07 -0400

    creado metodo para crear cuadrado magico aleatorio

--- src/MagicScuare.java
+++ src/MagicScuare.java
@@ -1,2 +1,31 @@
+import java.util.Collections;
+import java.util.Arrays;
 public class MagicScuare {
+    public static void crear_cuadrado(){
+        Integer[] lista_numeros = new Integer[9];
+        for (int i = 0; i < lista_numeros.length; i++) {
+            lista_numeros[i] = i+1;
+        }
+        Collections.shuffle(Arrays.asList(lista_numeros));
+        int[][] cuadrado = new int[3][3];
+        int contador = 0;
+        for (int i = 0; i < cuadrado.length; i++) {
+            for (int j = 0; j < cuadrado[i].length; j++) {
+                cuadrado[i][j] = lista_numeros[contador];
+                contador++;
+            }
+        }
+        imprimir(cuadrado);
+    }
+    public static void main(String[] args) {
+        crear_cuadrado();
+    }
+    public static void imprimir(int[][] cuadrado){
+        for (int i = 0; i < cuadrado.length; i++) {
+            for (int j = 0; j < cuadrado[i].length; j++) {
+                System.out.print(cuadrado[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
